d3f69a68297b8bc6315ebdac89ecb3c75f2f80d1
AndrewGallegos <andrew.gallegos.professional@gmail.com>
2019-08-12 16:20:39 -0600

    removed unnecessary selectors

--- pageObjects/wantedQueriesPage.js
+++ pageObjects/wantedQueriesPage.js
@@ -1,9 +1,6 @@
 module.exports = {
     url: 'http://localhost:3000/#/enter',
     elements:{
-        //homepage
-        menuBtn: '.bm-burger-button',
-        enterWanted: '[name=enterOption]',
         //fields
         header: '[name=hdrInput]',
         mke: '[name=mkeInput]',
